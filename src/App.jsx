5a887243ed01e52b67913dc6744cfb6894f49fd8
Tim Moss <tmoss726@hotmail.com>
2024-02-06 21:07:26 -0500

    React router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { useState } from 'react';
-import './styles/App.css';
 import Layout from './components/layout';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Layout>
-        <h1>Vite + React</h1>
-        <div className='card'>
-          <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-      </Layout>
+      <Layout />
     </>
   );
 }
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,31 +1,38 @@
 /* eslint-disable react/prop-types */
+import { Link, Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import Home from '../pages/Home';
+import Projects from '../pages/Projects';
+import Resume from '../pages/Resume';
+import Contact from '../pages/Contact';
+
 const Layout = ({ children }) => {
   return (
     <>
-      <nav>
-        <ul className='flex flex-row justify-center gap-4'>
-          <li>
-            <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-              About
-            </a>
-          </li>
-          <li>
-            <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-              Resume
-            </a>
-          </li>
-          <li>
-            <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-              Projects
-            </a>
-          </li>
-          <li>
-            <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <Router>
+        <nav className='my-8'>
+          <ul className='flex flex-row justify-center gap-4'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/projects'>Projects</Link>
+            </li>
+            <li>
+              <Link to='/resume'>Resume</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>
       {children}
     </>
   );
